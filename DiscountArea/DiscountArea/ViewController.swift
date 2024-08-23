c71c25ce5232bfb595eecbffe7d392b6ca899211
楊芮瑊 <rui@yangruijiandeMacBook-Pro.local>
2024-08-23 16:05:05 +0800

    FEAT: Established API Method

--- DiscountArea/DiscountArea/ViewController.swift
+++ DiscountArea/DiscountArea/ViewController.swift
@@ -3,12 +3,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var httpRequestManager = HTTPRequestManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        httpRequestManager.fetchPageData()
+        httpRequestManager.fetchProductData(productList: ["2247",
+                                                          "38965",
+                                                          "138901"])
     }
-
-
+    
 }
 
